e576be949ae27cabc374c89917a2cf8f854541aa
Tomas Dolezal <tomas.dolezal@gmail.com>
2016-04-08 09:14:57 +0200

    Initial version commit

---
+++ src/ServiceFabric.Extensions.Configuration/ServiceFabricConfigurationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using ServiceFabric.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Fabric.Description;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.Configuration
+{
+    public static class ServiceFabricConfigurationExtensions
+    {
+        public static IConfigurationBuilder AddFabricConfiguration(this IConfigurationBuilder configurationBuilder, ConfigurationSettings fabricConfig)
+        {
+            configurationBuilder.Add(new ServiceFabricConfigurationProvider(fabricConfig));
+            return configurationBuilder;
+        }
+
+        public static IConfigurationBuilder AddFabricConfiguration(this IConfigurationBuilder configurationBuilder, string configurationPackageName)
+        {
+            var config = FabricRuntime.GetActivationContext()?.GetConfigurationPackageObject(configurationPackageName);
+            if (config == null)
+                throw new ArgumentException($"Configuration package '{configurationPackageName}' was not found.", nameof(configurationPackageName));
+
+            return AddFabricConfiguration(configurationBuilder, config.Settings);
+        }
+    }
+}
---
+++ src/ServiceFabric.Extensions.Configuration/ServiceFabricConfigurationProvider.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Fabric.Description;
+using System.Linq;
+using System.Security;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ServiceFabric.Extensions.Configuration
+{
+    public class ServiceFabricConfigurationProvider : ConfigurationProvider
+    {
+        readonly ConfigurationSettings fabricConfig;
+
+        public ServiceFabricConfigurationProvider(ConfigurationSettings fabricConfig)
+        {
+            if (fabricConfig == null)
+                throw new ArgumentNullException(nameof(fabricConfig));
+
+            this.fabricConfig = fabricConfig;
+        }
+
+        public override void Load()
+        {
+            var data = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+            foreach (var section in fabricConfig.Sections)
+            {
+                var sectionPrefix = section.Name + Constants.KeyDelimiter;
+
+                foreach (var parameter in section.Parameters)
+                {
+                    var key = sectionPrefix + parameter.Name;
+                    var value = parameter.IsEncrypted ? SecureStringToString(parameter.DecryptValue()) : parameter.Value;
+
+                    if (data.ContainsKey(key))
+                        throw new FormatException($"Key '{key}' already exists");
+                    data[key] = value;
+                }
+            }
+
+            Data = data;
+        }
+
+        static string SecureStringToString(SecureString value)
+        {
+            IntPtr bstr = Marshal.SecureStringToBSTR(value);
+            try
+            {
+                return Marshal.PtrToStringBSTR(bstr);
+            }
+            finally
+            {
+                Marshal.FreeBSTR(bstr);
+            }
+        }
+    }
+}
